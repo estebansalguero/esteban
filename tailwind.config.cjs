019579ced3a921d6b1efbafffe9b28d85d296cda
estebansalguero <jesalguero6v@gmail.com>
2023-01-04 23:45:41 -0600

    Feat: initial page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        esteban_blue: "#0F172A",
+        esteban_blue_light: "#5d6e96",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
