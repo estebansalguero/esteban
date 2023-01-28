c7a6846e32526c80d1b752f38d58689c886a5165
estebansalguero <jesalguero6v@gmail.com>
2023-01-28 13:04:01 -0600

    Feat: using Nextjs

---
+++ components/Header.tsx
@@ -0,0 +1,103 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+import Icon from "../public/Vinyl.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <Popover className="relative bg-esteban_blue">
+        <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <Link href="/">
+              <Image
+                className="h-8 w-auto sm:h-10"
+                src={Icon}
+                width={50}
+                height={50}
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="-my-2 -mr-2 md:hidden">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-esteban_blue hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+            <Link
+              href="/resume"
+              className="text-base font-medium text-white hover:text-gray-50"
+            >
+              Resume
+            </Link>
+            <Link
+              href="/contact"
+              className="text-base font-medium text-white hover:text-gray-50"
+            >
+              Contact
+            </Link>
+          </Popover.Group>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="absolute inset-x-0 top-0 z-30 origin-top-right transform p-2 transition md:hidden"
+          >
+            <div className="divide-y-2 divide-gray-50 rounded-lg bg-esteban_blue shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="px-5 pt-5 pb-6">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <Image
+                      className="h-8 w-auto"
+                      src={Icon}
+                      width={50}
+                      height={50}
+                      alt="Your Company"
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-esteban_blue hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <div className="py-6 px-5">
+                    <div className="grid grid-cols-2 gap-4">
+                      <Link
+                        href="/resume"
+                        className="text-base font-medium text-white hover:text-gray-50"
+                      >
+                        Resume
+                      </Link>
+                      <Link
+                        href="/contact"
+                        className="text-base font-medium text-white hover:text-gray-50"
+                      >
+                        Contact
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+    </header>
+  );
+};
---
+++ pages/resume.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+
+const Resume = () => {
+  return (
+    <div className="bg-esteban_blue">
+      <Header />
+      <h1>Resume</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Resume;
